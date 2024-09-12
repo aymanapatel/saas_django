e04f30b138dda84b66b520d2e9350aae6a2e7bd4
Ayman Patel <Ayman.Patel@mastercard.com>
2024-09-12 17:39:27 +0530

    feat: add template

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from datetime import  datetime
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Hello world")
+    # return HttpResponse("Hello world")
+    return render(request, 'home/hello_world.html', {'today': datetime.today()})
