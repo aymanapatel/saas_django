ef9bc434816554b51f90ac7d55d48821ed7c8d42
Ayman Patel <Ayman.Patel@mastercard.com>
2024-09-12 18:44:13 +0530

    feat: add ORM

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
+from home.models import Product
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name',)
+    Product.objects.first(produ)
+
+
+admin.site.register(models.Product, ProductAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=10)
+    product_desc = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
